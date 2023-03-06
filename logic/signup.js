fad7ff84d70f96f059c160e7c0634e2feccdba5e
Wael Dghim <104037074+Wael-Dghim@users.noreply.github.com>
2023-03-06 13:34:28 +0100

    signup page/functionality done

---
+++ logic/signup.js
@@ -0,0 +1,24 @@
+$("form").submit(function (event) {
+  event.preventDefault();
+  var usernameTaken = window.users.find(function (user) {
+    return $("#username").val() == user.username;
+  });
+  if (usernameTaken) {
+    alert("Please enter a different username");
+  } else {
+    if (
+      $("#pass").val() === $("#confirm-password").val() ||
+      $("#pass").val().length > 6
+    ) {
+      var newUser = {
+        username: $("#username").val(),
+        password: $("#pass").val(),
+      };
+      window.users.push(newUser);
+    } else {
+      alert(
+        "Please make sure your password is longer than 6 characters and is identical to the 'Confirm Password' field"
+      );
+    }
+  }
+});
