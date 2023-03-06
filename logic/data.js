01d765fae8df95e15b159dc9e8a7eaacca004fb6
Wael Dghim <104037074+Wael-Dghim@users.noreply.github.com>
2023-03-06 16:16:03 +0100

    prof or student radio added

--- logic/data.js
+++ logic/data.js
@@ -2,37 +2,57 @@ window.users = [
   {
     username: "admin",
     password: "password",
+    isProfessor: false,
+    courses: [],
   },
   {
     username: "admin",
     password: "password",
+    isProfessor: false,
+    courses: [],
   },
   {
     username: "admin",
     password: "password",
+    isProfessor: false,
+    courses: [],
   },
   {
     username: "admin",
     password: "password",
+    isProfessor: false,
+    courses: [],
   },
   {
     username: "admin",
     password: "password",
+    isProfessor: false,
+    courses: [],
   },
   {
     username: "admin",
     password: "password",
+    isProfessor: false,
+    courses: [],
   },
   {
     username: "admin",
     password: "password",
+    isProfessor: false,
+    courses: [],
   },
   {
     username: "admin",
     password: "password",
+    isProfessor: false,
+    courses: [],
   },
   {
     username: "admin",
     password: "password",
+    isProfessor: false,
+    courses: [],
   },
 ];
+
+window.courses = []
