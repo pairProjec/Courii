f91fbd093294715459b172516ca6eff20301a6e3
Wael Dghim <104037074+Wael-Dghim@users.noreply.github.com>
2023-03-07 15:51:23 +0100

    professor page done

--- logic/professor.js
+++ logic/professor.js
@@ -6,23 +6,29 @@ $(document).ready(function () {
   var professor = {
     username: "admin",
     courses: [
-      { name: "HAHA 1", description: "Course", subject: "math" },
-      { name: "HAHA 2", description: "Course", subject: "math" },
-      { name: "HAHA 3", description: "Course", subject: "math" },
+      { name: "Course 1", description: "Description", subject: "subject" },
+      { name: "Course 2", description: "Description", subject: "subject" },
+      { name: "Course 3", description: "Description", subject: "subject" },
     ],
   };
 
   var $title = $(`<h1>Welcome to ${professor.username}'s profile </h1>`);
+  var $subtitle = $(
+    `<h2 id="sub">This is what ${professor.username} teaches</h2>`
+  );
+  $("body").prepend($subtitle);
   $("body").prepend($title);
   var $courseList = $("<ul id='courses'></ul>");
   for (var i = 0; i < professor.courses.length; i++) {
     var $content = $(
-      `<div class='course'><li>${professor.courses[i].name} </li></div>`
+      `<div class='course'><li><span class="name">${professor.courses[i].name} </span></li></div>`
     );
     $($content).append(
-      $(`<span>${professor.courses[i].description}</span><br />`)
+      $(`<span class="desc">${professor.courses[i].description}</span><br />`)
     );
-    $($content).append($(`<span>${professor.courses[i].subject}</span>`));
+    $($content).append(
+      $(`<span class="subj">${professor.courses[i].subject}</span>`)
+    );
     $($courseList).append($content);
   }
 
