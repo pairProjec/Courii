82a565ed14bf7ec2b09853d1342f11d2a9d1c37c
Wael Dghim <104037074+Wael-Dghim@users.noreply.github.com>
2023-03-07 13:27:33 +0100

    professor can add courses

--- logic/professor.js
+++ logic/professor.js
@@ -1,3 +1,7 @@
+//  var professor = window.users.find(function (user) {
+//     return user.username === window.localStorage.getItem("professor");
+//   });
+
 $(document).ready(function () {
   var professor = {
     username: "admin",
@@ -7,9 +11,7 @@ $(document).ready(function () {
       { name: "HAHA 3", description: "Course", subject: "math" },
     ],
   };
-  //   window.users.find(function (user) {
-  //     return user.username === window.localStorage.getItem("professor");
-  //   });
+
   var $title = $(`<h1>Welcome to ${professor.username}'s profile </h1>`);
   $("body").prepend($title);
   var $courseList = $("<ul id='courses'></ul>");
@@ -25,4 +27,29 @@ $(document).ready(function () {
   }
 
   $("#container").append($courseList);
+});
+
+$("#add").click(function (e) {
+  e.preventDefault();
+  var isOnline = $('input[name="online"]:checked').val();
+  var professor = {
+    username: "admin",
+    courses: [
+      { name: "HAHA 1", description: "Course", subject: "math" },
+      { name: "HAHA 2", description: "Course", subject: "math" },
+      { name: "HAHA 3", description: "Course", subject: "math" },
+    ],
+  };
+
+  var newCourse = {
+    name: $("#name").val(),
+    professor: professor.username,
+    level: $("#level").val(),
+    subject: $("#subject").val(),
+    description: $("#desc").val(),
+    isOnline: isOnline === "online",
+  };
+
+  window.courses.push(newCourse);
+  window.users[window.users.indexOf(professor)].courses.push(newCourse);
 });
